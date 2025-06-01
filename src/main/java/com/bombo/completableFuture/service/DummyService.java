ffdc20f7ef8cbaf506cc1ca743c326004200b8e1
TI-Bombo <bombo96@chungwoon.ac.kr>
2025-06-01 22:37:44 +0900

    실행 로그 수정

--- src/main/java/com/bombo/completableFuture/service/DummyService.java
+++ src/main/java/com/bombo/completableFuture/service/DummyService.java
@@ -16,36 +16,40 @@ public class DummyService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        log.info("Method a completed in thread {}", Thread.currentThread().getName());
         return "A";
     }
 
     public String b() {
-        log.info("Thread {} is executing method a", Thread.currentThread().getName());
+        log.info("Thread {} is executing method b", Thread.currentThread().getName());
         try {
             Thread.sleep(300);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        log.info("Method b completed in thread {}", Thread.currentThread().getName());
         return "B";
     }
 
     public String c() {
-        log.info("Thread {} is executing method a", Thread.currentThread().getName());
+        log.info("Thread {} is executing method c", Thread.currentThread().getName());
         try {
             Thread.sleep(300);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        log.info("Method c completed in thread {}", Thread.currentThread().getName());
         return "C";
     }
 
     public String d() {
-        log.info("Thread {} is executing method a", Thread.currentThread().getName());
+        log.info("Thread {} is executing method d", Thread.currentThread().getName());
         try {
             Thread.sleep(300);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        log.info("Method d completed in thread {}", Thread.currentThread().getName());
         return "D";
     }
 
